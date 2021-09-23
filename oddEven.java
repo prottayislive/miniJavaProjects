7567fd882c96c7d2519aec6b0b75ba22ab408ec9
Prottay Karim <70666023+prottayislive@users.noreply.github.com>
2021-09-23 21:22:36 +1000

    Added odd or even finder

---
+++ oddEven.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class oddEven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+        if (num % 2 == 0){
+            System.out.println(num + " is an even number");
+        }
+        else {
+            System.out.println(num + " is a odd number");
+        }
+    }
+}
